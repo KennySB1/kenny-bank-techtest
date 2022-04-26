fa411de16022f6b19deb8dc505cb040089a6b001
KennySB1 <kennysweeneybaird1@gmail.com>
2022-04-26 16:43:02 +0100

    Added functionality and tests for bank statements on withdrawing and depositing

--- accountStatements.js
+++ accountStatements.js
@@ -6,8 +6,32 @@ class AccountStatements {
     statements() {
         let statements = ""
         this.accountStatements.forEach(statement => statements += `${statement}\n`)
-
+        console.log(statements)
         return statements
     }
+
+    newStatement(balance, amount, transactionType) {
+        let statementToAdd = ""
+        if (transactionType === "deposit") {
+            statementToAdd += `${this.#currentDate()} || ${amount} || || ${balance}`
+            this.accountStatements.push(statementToAdd)
+            console.log(statementToAdd)
+        } else if (transactionType === "withdrawal") {
+            statementToAdd += `${this.#currentDate()} || || ${amount} || ${balance}`
+            this.accountStatements.push(statementToAdd)
+            console.log(statementToAdd)
+        }
+    }
+
+    #currentDate() {
+        let dateObj = new Date();
+        let year = dateObj.getFullYear()
+        let month = dateObj.getUTCMonth()
+        let day = dateObj.getUTCDate()
+
+        return year + "/" + month + "/" + day
+
+    }
+
 }
 module.exports = AccountStatements
--- accountStatements.test.js
+++ accountStatements.test.js
@@ -3,3 +3,15 @@ test('statements should start with only formatting', () => {
     const accountStatements = new AccountStatements
     expect(accountStatements.statements()).toBe("date || credit || debit || balance\n")
 })
+
+test('statements should include a new transaction after depositing', () => {
+    const accountStatements = new AccountStatements
+    accountStatements.newStatement(1000, 500, "deposit")
+    expect(accountStatements.statements()).toBe("date || credit || debit || balance\n2022/3/26 || 500 || || 1000\n")
+})
+
+test('statements should include a new transaction after withdrawing', () => {
+    const accountStatements = new AccountStatements
+    accountStatements.newStatement(1000, 500, "withdrawal")
+    expect(accountStatements.statements()).toBe("date || credit || debit || balance\n2022/3/26 || || 500 || 1000\n")
+})
